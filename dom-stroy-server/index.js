c4979b7832b52bd084a224bfc7a3adbe96c24b31
bloowb0und <bloowbound@gmail.com>
2022-05-16 22:20:53 +0300

    Modified folders, added sqlite.

---
+++ dom-stroy-server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const catalog = require('./routes/api/catalog.js');
+app.use('/api/catalog', catalog);
+
+const admins = require('./routes/api/admins.js');
+app.use('/api/admins', admins);
+
+const requests = require('./routes/api/requests.js');
+app.use('/api/requests', requests);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
